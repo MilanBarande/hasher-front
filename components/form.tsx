6a8733fcab3e7f3d33a6c454fb88916f5273723b
MilanBarande <milan.barande@gmail.com>
2021-12-11 14:29:42 +0100

    remove boilerplate and create Form.tsx

---
+++ components/form.tsx
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+import styles from '../styles/Form.module.scss'
+
+const Form = () => {
+  return (
+    <div className={styles.container}>
+      <div />
+    </div>
+  )
+}
+
+export default Form
