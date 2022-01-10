583999e40fc0e5dfb7cea63e75e4d103ebac2abf
MilanBarande <milan.barande@gmail.com>
2022-01-10 17:25:05 +0100

    update title

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,7 @@ const Home: NextPage = () => {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Archive level 9 hash</title>
+        <title>Archive level 8</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
